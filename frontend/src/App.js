c0b87a9b6c31a4f73128d4bf6bf2d7310343d5b1
phucdo <phucdt@imt-soft.com>
2023-08-21 20:33:42 +0700

    cc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,36 +4,37 @@ import HomePage from "./page/HomePage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-        {/* <Route path="/" element={<HomePage />} />
-        <Route path="/product/:slug" element={<ProductDetails />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/category/:slug" element={<CategoryProduct />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="user" element={<Dashboard />} />
-          <Route path="user/orders" element={<Orders />} />
-          <Route path="user/profile" element={<Profile />} />
-        </Route>
-        <Route path="/dashboard" element={<AdminRoute />}>
-          <Route path="admin" element={<AdminDashboard />} />
-          <Route path="admin/create-category" element={<CreateCategory />} />
-          <Route path="admin/create-product" element={<CreateProduct />} />
-          <Route path="admin/product/:slug" element={<UpdateProduct />} />
-          <Route path="admin/products" element={<Products />} />
-          <Route path="admin/users" element={<Users />} />
-          <Route path="admin/orders" element={<AdminOrders />} />
-        </Route>
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPasssword />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/policy" element={<Policy />} />
-        <Route path="*" element={<Pagenotfound />} /> */}
-      </Routes>
+    <h1>cc</h1>
+    // <Routes>
+    //   <Route path="/" element={<HomePage />} />
+    //     <Route path="/" element={<HomePage />} />
+    //     <Route path="/product/:slug" element={<ProductDetails />} />
+    //     <Route path="/categories" element={<Categories />} />
+    //     <Route path="/cart" element={<CartPage />} />
+    //     <Route path="/category/:slug" element={<CategoryProduct />} />
+    //     <Route path="/search" element={<Search />} />
+    //     <Route path="/dashboard" element={<PrivateRoute />}>
+    //       <Route path="user" element={<Dashboard />} />
+    //       <Route path="user/orders" element={<Orders />} />
+    //       <Route path="user/profile" element={<Profile />} />
+    //     </Route>
+    //     <Route path="/dashboard" element={<AdminRoute />}>
+    //       <Route path="admin" element={<AdminDashboard />} />
+    //       <Route path="admin/create-category" element={<CreateCategory />} />
+    //       <Route path="admin/create-product" element={<CreateProduct />} />
+    //       <Route path="admin/product/:slug" element={<UpdateProduct />} />
+    //       <Route path="admin/products" element={<Products />} />
+    //       <Route path="admin/users" element={<Users />} />
+    //       <Route path="admin/orders" element={<AdminOrders />} />
+    //     </Route>
+    //     <Route path="/register" element={<Register />} />
+    //     <Route path="/forgot-password" element={<ForgotPasssword />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //     <Route path="/policy" element={<Policy />} />
+    //     <Route path="*" element={<Pagenotfound />} />
+    //   </Routes>
   );
 }
 
