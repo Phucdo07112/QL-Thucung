d36d8c11556d8cd7e2525db7ceb4485283ab9a73
phucdo <phucdt@imt-soft.com>
2023-08-23 19:27:59 +0700

    push

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='container'>
+      <nav className='flex items-center'>
+        <img src='/images/Logo.png' alt=''/>
+        <div>
+          {/* <Link to={"/a"} className='text-red-500 text-lg' >home</Link> */}
+        </div>
+      </nav>
+    </header>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Layout>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Layout>
   </React.StrictMode>
 );
 
