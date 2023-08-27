8f587c441c1588a39fed6af983f42a4fe07d66de
Hoang <85085613+nmhoang2002@users.noreply.github.com>
2023-08-27 10:39:35 +0700

    header

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./headercss/header.css"
+import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 const Header = () => {
   return (
-    <header className='container'>
-      <nav className='flex items-center'>
-        <img src='/images/Logo.png' alt=''/>
-        <div>
-          {/* <Link to={"/a"} className='text-red-500 text-lg' >home</Link> */}
-        </div>
-      </nav>
-    </header>
+    
+        <header >
+          <div className='container flex items-center justify-between'>
+          <div>
+            <img src="/images/Logo.png" alt="Logo"/>
+          </div>
+          
+      <ul>      
+        <li><a href='#' class="active">Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Blog</a></li>
+        <li><a href='#'>Gallery</a></li>
+        <li><a href='#'>Contact</a></li>
+      </ul>
+
+      <div  className='flex items-center gap-4' >
+      <AiOutlineSearch/>
+      <AiOutlineShoppingCart/>
+      <div id="login">
+      <a href='#'>Login</a>
+      </div>
+      </div>
+      
+          </div>
+        </header>
+    
+    
+
+
+
+
+
   )
 }
 
