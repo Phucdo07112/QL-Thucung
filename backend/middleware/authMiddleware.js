43de2c9b8a60e81da6a9c29d7b7a212deeb0d4b6
phucdo <phucdt@imt-soft.com>
2023-10-08 23:25:04 +0700

    fig bug

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,7 +28,6 @@ const authMiddleWare = (req, res, next) => {
 const authUserMiddleWare = (req, res, next) => {
     const token = req.headers.token.split(' ')[1]
     const userId = req.params.id
-    console.log('req.headers.token',req.headers.token);
     jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
         if(err) {
             return res.status(404).json({
