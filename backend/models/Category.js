97d3d5e13c00d0b58caaa98706d8b886b7fa2544
phucdo <phucdt@imt-soft.com>
2023-12-05 13:13:26 +0700

    category-admin-backend

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,22 +1,22 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const CategorySchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-//     sect: {
-//       type: String,
-//       required: true,
-//     },
-//     image: {
-//       type: String,
-//     },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    sect: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// module.exports = Category = mongoose.model("Category", CategorySchema);
+module.exports = Category = mongoose.model("Category", CategorySchema);
