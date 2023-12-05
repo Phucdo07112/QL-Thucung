6fda87e07151566b179cac43179500d16a57fc00
phucdo <phucdt@imt-soft.com>
2023-12-05 12:35:11 +0700

    cart-page-fullstack

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -1,23 +1,23 @@
 const OrderService = require('../services/OrderService')
 
-// const createOrder = async (req, res) => {
-//     try { 
-//         const { paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone } = req.body
-//         console.log("createOrder",{paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone});
-//         if (!paymentMethod || !itemsPrice || !totalPrice || !fullName || !address || !city || !phone) {
-//             return res.status(200).json({
-//                 status: 'ERR',
-//                 message: 'The input is required'
-//             })
-//         }
-//         const response = await OrderService.createOrder(req.body)
-//         return res.status(200).json(response)
-//     } catch (e) {
-//         return res.status(404).json({
-//             message: e
-//         })
-//     }
-// }
+const createOrder = async (req, res) => {
+    try { 
+        const { paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone } = req.body
+        console.log("createOrder",{paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone});
+        if (!paymentMethod || !itemsPrice || !totalPrice || !fullName || !address || !city || !phone) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        }
+        const response = await OrderService.createOrder(req.body)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
 
 const getAllOrderDetails = async (req, res) => {
     try {
