4563a91d5aeb21cabd34821288e819a148269641
Hoang <85085613+nmhoang2002@users.noreply.github.com>
2023-12-10 20:46:43 +0700

    cateAdminFrontend

--- frontend/src/components/AdminCategory/AdminCategory.js
+++ frontend/src/components/AdminCategory/AdminCategory.js
@@ -152,125 +152,125 @@ const AdminCategory = () => {
     );
   }
 
-  // const renderAction = () => {
-  //   return (
-  //     <div>
-  //       <DeleteOutlined
-  //         style={{
-  //           fontSize: "20px",
-  //           color: "white",
-  //           backgroundColor: "red",
-  //           padding: "8px",
-  //           borderRadius: "10px",
-  //           cursor: "pointer",
-  //         }}
-  //         onClick={() => setIsModalOpenDelete(true)}
-  //       />
-  //       <EditOutlined
-  //         style={{
-  //           fontSize: "20px",
-  //           color: "white",
-  //           backgroundColor: "yellow",
-  //           padding: "8px",
-  //           borderRadius: "10px",
-  //           marginLeft: "10px",
-  //           cursor: "pointer",
-  //         }}
-  //         onClick={handleDetailsCategory}
-  //       />
-  //     </div>
-  //   );
-  // };
+  const renderAction = () => {
+    return (
+      <div>
+        <DeleteOutlined
+          style={{
+            fontSize: "20px",
+            color: "white",
+            backgroundColor: "red",
+            padding: "8px",
+            borderRadius: "10px",
+            cursor: "pointer",
+          }}
+          onClick={() => setIsModalOpenDelete(true)}
+        />
+        <EditOutlined
+          style={{
+            fontSize: "20px",
+            color: "white",
+            backgroundColor: "yellow",
+            padding: "8px",
+            borderRadius: "10px",
+            marginLeft: "10px",
+            cursor: "pointer",
+          }}
+          onClick={handleDetailsCategory}
+        />
+      </div>
+    );
+  };
 
-  // const handleSearch = (selectedKeys, confirm, dataIndex) => {
-  //   confirm();
-  //   // setSearchText(selectedKeys[0]);
-  //   // setSearchedColumn(dataIndex);
-  // };
-  // const handleReset = (clearFilters) => {
-  //   clearFilters();
-  //   // setSearchText('');
-  // };
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    // setSearchText(selectedKeys[0]);
+    // setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    // setSearchText('');
+  };
 
-  // const getColumnSearchProps = (dataIndex) => ({
-  //   filterDropdown: ({
-  //     setSelectedKeys,
-  //     selectedKeys,
-  //     confirm,
-  //     clearFilters,
-  //   }) => (
-  //     <div
-  //       style={{
-  //         padding: 8,
-  //       }}
-  //       onKeyDown={(e) => e.stopPropagation()}
-  //     >
-  //       <InputComponent
-  //         ref={searchInput}
-  //         placeholder={`Search ${dataIndex}`}
-  //         value={selectedKeys[0]}
-  //         onChange={(e) =>
-  //           setSelectedKeys(e.target.value ? [e.target.value] : [])
-  //         }
-  //         onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-  //         style={{
-  //           marginBottom: 8,
-  //           display: "block",
-  //         }}
-  //       />
-  //       <Space>
-  //         <Button
-  //           type="primary"
-  //           onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-  //           icon={<SearchOutlined />}
-  //           size="small"
-  //           style={{
-  //             width: 90,
-  //           }}
-  //         >
-  //           Search
-  //         </Button>
-  //         <Button
-  //           onClick={() => clearFilters && handleReset(clearFilters)}
-  //           size="small"
-  //           style={{
-  //             width: 90,
-  //           }}
-  //         >
-  //           Reset
-  //         </Button>
-  //       </Space>
-  //     </div>
-  //   ),
-  //   filterIcon: (filtered) => (
-  //     <SearchOutlined
-  //       style={{
-  //         color: filtered ? "#1890ff" : undefined,
-  //       }}
-  //     />
-  //   ),
-  //   onFilter: (value, record) =>
-  //     record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-  //   onFilterDropdownOpenChange: (visible) => {
-  //     if (visible) {
-  //       setTimeout(() => searchInput.current?.select(), 100);
-  //     }
-  //   },
-  //   // render: (text) =>
-  //   //   searchedColumn === dataIndex ? (
-  //   //     // <Highlighter
-  //   //     //   highlightStyle={{
-  //   //     //     backgroundColor: '#ffc069',
-  //   //     //     padding: 0,
-  //   //     //   }}
-  //   //     //   searchWords={[searchText]}
-  //   //     //   autoEscape
-  //   //     //   textToHighlight={text ? text.toString() : ''}
-  //   //     // />
-  //   //   ) : (
-  //   //     text
-  //   //   ),
-  // });
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <InputComponent
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: "block",
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? "#1890ff" : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    // render: (text) =>
+    //   searchedColumn === dataIndex ? (
+    //     // <Highlighter
+    //     //   highlightStyle={{
+    //     //     backgroundColor: '#ffc069',
+    //     //     padding: 0,
+    //     //   }}
+    //     //   searchWords={[searchText]}
+    //     //   autoEscape
+    //     //   textToHighlight={text ? text.toString() : ''}
+    //     // />
+    //   ) : (
+    //     text
+    //   ),
+  });
 
   const columns = [
     {
